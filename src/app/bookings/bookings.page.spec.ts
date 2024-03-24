bccea3bc3429ee33a0eb81f28532f5c7f0436aa5
Noranisah <22ftt1492@student.pb.edu.bn>
2024-03-24 15:56:51 +0800

    booking spec updated

--- src/app/bookings/bookings.page.spec.ts
+++ src/app/bookings/bookings.page.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BookingsPage } from './bookings.page';
 
-describe('BookingsPage', () => {
+describe('BookingPage', () => {
   let component: BookingsPage;
   let fixture: ComponentFixture<BookingsPage>;
 
