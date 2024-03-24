aaaeffbb0ff37079d8201980f5160bf374cc3fbd
Noranisah <22ftt1492@student.pb.edu.bn>
2024-03-24 16:20:03 +0800

    app chnages and updated one

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { TabsPage } from './tabs/tabs.page';
 
 export const routes: Routes = [
   {
+    path: 'auth',
+    loadComponent: () => import('./auth/auth.page').then((m) => m.AuthPage),
+  },
+  {
     path: '',
     redirectTo: 'auth',
     pathMatch: 'full',
@@ -11,21 +15,24 @@ export const routes: Routes = [
     path: 'auth',
     loadComponent: () => import('./auth/auth.page').then((m) => m.AuthPage),
   },
-  // {
-  //   path: 'places',
-  //   loadComponent: () =>
-  //     import('./places/places.page').then((m) => m.PlacesPage),
-  // },
+  {
+    path: 'places',
+    loadComponent: () =>
+      import('./places/places.page').then((m) => m.PlacesPage),
+  },
   {
     path: 'place-detail',
     loadComponent: () =>
-      import('./place-detail/place-detail.page').then((m) => m.PlaceDetailPage),
+      import('./places/place-detail/place-detail.page').then(
+        (m) => m.PlaceDetailPage
+      ),
   },
   {
     path: 'offers',
     loadComponent: () =>
       import('./offers/offers.page').then((m) => m.OffersPage),
   },
+
   {
     path: 'tabs',
     component: TabsPage,
@@ -47,12 +54,36 @@ export const routes: Routes = [
     redirectTo: '/tabs/places',
     pathMatch: 'full',
   },
+
   {
-    path: 'tabs',
-    loadComponent: () => import('./tabs/tabs.page').then((m) => m.TabsPage),
+    path: 'offer-details',
+    loadComponent: () =>
+      import('./offers/offer-details/offer-details.page').then(
+        (m) => m.OfferDetailsPage
+      ),
+  },
+  {
+    path: 'booking',
+    loadComponent: () =>
+      import('./bookings/bookings.page').then((m) => m.BookingsPage),
+  },
+  {
+    path: 'offer-bookings',
+    loadComponent: () =>
+      import('./offers/offer-booking/offer-booking.page').then(
+        (m) => m.OfferBookingPage
+      ),
+  },
+
+  {
+    path: 'new-offers',
+    loadComponent: () =>
+      import('./offers/new-offers/new-offers.page').then(
+        (m) => m.NewOffersPage
+      ),
   },
   {
     path: 'tabs',
-    loadComponent: () => import('./tabs/tabs.page').then( m => m.TabsPage)
+    loadComponent: () => import('./tabs/tabs.page').then((m) => m.TabsPage),
   },
 ];
