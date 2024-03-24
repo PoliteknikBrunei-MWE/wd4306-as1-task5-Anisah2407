ccdd216201c8ef535a8c0480a75ac10ef6542667
Noranisah <22ftt1492@student.pb.edu.bn>
2024-03-24 16:12:55 +0800

    places updated

--- src/app/places/place-detail/place-detail.page.ts
+++ src/app/places/place-detail/place-detail.page.ts
@@ -2,19 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { cart } from 'ionicons/icons';
 
 @Component({
   selector: 'app-place-detail',
   templateUrl: './place-detail.page.html',
   styleUrls: ['./place-detail.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class PlaceDetailPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({
+      cart,
+    });
   }
 
+  ngOnInit() {}
 }
